8e5f2983bce988c5ea93327808f6d33a987764bd
Zeztron <pateltharsh@gmail.com>
2019-06-15 16:47:07 -0400

    styled Card and implemented

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text } from 'react-native';
+import Card from './Card';
 
 const AlbumDetail = ({ album: { title } }) => {
 	return (
-		<View>
+		<Card>
 			<Text>{title}</Text>
-		</View>
+		</Card>
   	);
 
 };
