af4257b5ad084f4895e057105c50b54ec5537be7
Zeztron <pateltharsh@gmail.com>
2019-06-15 17:06:59 -0400

    added text container

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = ({ album: { title } }) => {
+const AlbumDetail = ({ album: { title , artist } }) => {
+	const { headerContentStyle } = styles;
+
 	return (
 		<Card>
 			<CardSection>
-				<Text>{title}</Text>
+				<View></View>
+				<View style={headerContentStyle}>
+					<Text>{title}</Text>
+					<Text>{artist}</Text>
+				</View>
 			</CardSection>
 		</Card>
   	);
+};
 
+const styles = {
+	headerContentStyle: {
+		flexDirection: 'column',
+		justifyContent: 'space-around'
+	}
 };
 
 export default AlbumDetail;
